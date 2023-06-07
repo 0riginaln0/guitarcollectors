81234ce0b39c90bc744271281c8ed37b04523741
Ryzh <gooddmitriy@mail.ru>
2023-06-07 13:46:07 +0300

    basic filter chain

--- src/main/java/com/example/guitarcollectors/controller/HomeController.java
+++ src/main/java/com/example/guitarcollectors/controller/HomeController.java
@@ -8,6 +8,16 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        return "Hello, world";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello, admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
         return "Hello, user";
     }
 }
---
+++ src/main/java/com/example/guitarcollectors/security/SpringSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.guitarcollectors.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("pwd")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("pwd")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+                    auth.requestMatchers("/user").hasRole("USER");
+                    auth.requestMatchers("/admin").hasRole("ADMIN");
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
