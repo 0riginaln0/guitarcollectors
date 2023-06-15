755f46ccf6b3518b9074b9ee5393dcba9b8b13bb
Ryzh <gooddmitriy@mail.ru>
2023-06-15 11:01:40 +0300

    forgot the main thing :)

--- src/main/java/com/example/guitarcollectors/config/JwtService.java
+++ src/main/java/com/example/guitarcollectors/config/JwtService.java
@@ -20,7 +20,6 @@ import java.util.function.Function;
 public class JwtService {
 
     private final JwtConfigProperties JWT_CONFIG_PROPERTIES;
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public JwtService(JwtConfigProperties jwtConfigProperties) {
         JWT_CONFIG_PROPERTIES = jwtConfigProperties;
